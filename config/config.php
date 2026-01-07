cf69236542b8f55788c41050982bbc6c599b3259
Arzaleph <zaaaleph@gmail.com>
2026-01-07 22:08:19 +0700

    fix config define DB

--- config/config.php
+++ config/config.php
@@ -1,4 +1,10 @@
 <?php
-session_start();
+// Jangan ada session_start di sini karena sudah ada di index.php
 define("BASE_URL", "http://localhost/umkm-gedong-meneng-baru");
 date_default_timezone_set("Asia/Jakarta");
+
+// Database Config
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "umkm_desa");
